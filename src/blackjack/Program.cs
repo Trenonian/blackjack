24d3c62cb992cdf2cbe0bd3e96d1cb6f5946d4bc
Michael Anderson <amon-ra@sbcglobal.net>
2016-03-27 22:57:37 -0500

    added some unit tests

--- src/blackjack/Program.cs
+++ src/blackjack/Program.cs
@@ -27,173 +27,195 @@ namespace blackjack
         Jack,
         Queen,
         King
-    } 
+    }
+
+    public class Card
+    {
+        public CardSuit Suit { get; private set; }
+        private int _value;
+        public int Score
+        {
+            get
+            {
+                if (_value < 10)
+                {
+                    return _value;
+                }
+                return 10;
+            }
+        }
+        private int Value
+        {
+            get
+            {
+                return _value;
+            }
+            set
+            {
+                if (value < 1 || value > 13)
+                {
+                    throw new ArgumentOutOfRangeException("Value");
+                }
+                _value = value;
+            }
+        }
+        public CardDisplay Display
+        {
+            get
+            {
+                if (Value == 1)
+                {
+                    return CardDisplay.Ace;
+                }
+                if (Value == 2)
+                {
+                    return CardDisplay.Two;
+                }
+                if (Value == 3)
+                {
+                    return CardDisplay.Three;
+                }
+                if (Value == 4)
+                {
+                    return CardDisplay.Four;
+                }
+                if (Value == 5)
+                {
+                    return CardDisplay.Five;
+                }
+                if (Value == 6)
+                {
+                    return CardDisplay.Six;
+                }
+                if (Value == 7)
+                {
+                    return CardDisplay.Seven;
+                }
+                if (Value == 8)
+                {
+                    return CardDisplay.Eight;
+                }
+                if (Value == 9)
+                {
+                    return CardDisplay.Nine;
+                }
+                if (Value == 10)
+                {
+                    return CardDisplay.Ten;
+                }
+                if (Value == 11)
+                {
+                    return CardDisplay.Jack;
+                }
+                if (Value == 12)
+                {
+                    return CardDisplay.Queen;
+                }
+                if (Value == 13)
+                {
+                    return CardDisplay.King;
+                }
+                throw new IndexOutOfRangeException("Value");
+            }
+        }
+        public Card(CardSuit suit, int val)
+        {
+            Suit = suit;
+            Value = val;
+        }
+    }
+
+
+    public class Deck
+    {
+        public static Random rand = new Random();
+
+        public List<Card> Cards { get; set; }
+
+        public void Deal(Hand hand)
+        {
+            Card dealCard = Cards.First();
+            Cards.Remove(dealCard);
+            hand.Cards.Add(dealCard);
+        }
+
+        public void Shuffle()
+        {
+            int n = Cards.Count;
+            while (n > 1)
+            {
+                n--;
+                int k = rand.Next(n + 1);
+                Card value = Cards[k];
+                Cards[k] = Cards[n];
+                Cards[n] = value;
+            }
+        }
+
+        public Deck()
+        {
+            Cards = new List<Card>();
+            for (int i = 1; i < 14; i++)
+            {
+                foreach (CardSuit suit in Enum.GetValues(typeof(CardSuit)))
+                {
+                    Cards.Add(new Card(suit, i));
+                }
+            }
+            Shuffle();
+        }
+    }
+
+    public class Hand
+    {
+        public string Name { get; set; }
+        public List<Card> Cards { get; set; }
+        public int LowAceCount { get; set; }
+        public int HandValue
+        {
+            get
+            {
+                int score = 0;
+                LowAceCount = Cards.FindAll(c => c.Display == CardDisplay.Ace).Count;
+                foreach (Card card in Cards)
+                {
+                    score += card.Score;
+                }
+                while (score < 12 && LowAceCount > 0)
+                {
+                    LowAceCount--;
+                    score += 10;
+                }
+                return score;
+            }
+        }
+        public void Display()
+        {
+            Console.WriteLine("{0}'s Hand", Name);
+            foreach (Card card in Cards)
+            {
+                Console.WriteLine(string.Format("{0} of {1}", card.Display, card.Suit));
+            }
+            Console.WriteLine(string.Format("Hand Value: {0}", HandValue));
+            Console.WriteLine();
+        }
+        public Hand(string name)
+        {
+            LowAceCount = 0;
+            Name = name;
+            Cards = new List<Card>();
+        }
+    }
 
     public class Program
     {
-
-        public class Card
-        {
-            public CardSuit Suit { get; private set; }
-            public int Value { get; private set; }
-            public CardDisplay Display
-            {
-                get
-                {
-                    if (Value == 1)
-                    {
-                        return CardDisplay.Ace;
-                    }
-                    if (Value == 2)
-                    {
-                        return CardDisplay.Two;
-                    }
-                    if (Value == 3)
-                    {
-                        return CardDisplay.Three;
-                    }
-                    if (Value == 4)
-                    {
-                        return CardDisplay.Four;
-                    }
-                    if (Value == 5)
-                    {
-                        return CardDisplay.Five;
-                    }
-                    if (Value == 6)
-                    {
-                        return CardDisplay.Six;
-                    }
-                    if (Value == 7)
-                    {
-                        return CardDisplay.Seven;
-                    }
-                    if (Value == 8)
-                    {
-                        return CardDisplay.Eight;
-                    }
-                    if (Value == 9)
-                    {
-                        return CardDisplay.Nine;
-                    }
-                    if (Value == 10)
-                    {
-                        return CardDisplay.Ten;
-                    }
-                    if (Value == 11)
-                    {
-                        return CardDisplay.Jack;
-                    }
-                    if (Value == 12)
-                    {
-                        return CardDisplay.Queen;
-                    }
-                    if (Value == 13)
-                    {
-                        return CardDisplay.King;
-                    }
-                    throw new IndexOutOfRangeException("Value");
-
-                }
-            }
-            public Card(CardSuit suit, int val)
-            {
-                Suit = suit;
-                Value = val;
-            }
-        }
-
-
-        public class Deck
-        {
-            public static Random rand = new Random();
-
-            public List<Card> Cards { get; set; }
-
-            public void Deal(Hand hand)
-            {
-                Card dealCard = Cards.First();
-                Cards.Remove(dealCard);
-                hand.Cards.Add(dealCard);
-            }
-
-            public void Shuffle()
-            {
-                int n = Cards.Count;
-                while (n > 1)
-                {
-                    n--;
-                    int k = rand.Next(n + 1);
-                    Card value = Cards[k];
-                    Cards[k] = Cards[n];
-                    Cards[n] = value;
-                }
-            }
-
-            public Deck()
-            {
-                Cards = new List<Card>();
-                for(int i = 1; i< 14; i++)
-                {
-                    foreach (CardSuit suit in Enum.GetValues(typeof(CardSuit)))
-                    {
-                        if (i > 10)
-                        {
-                            Cards.Add(new Card(suit, 10));
-                        }
-                        else
-                        {
-                            Cards.Add(new Card(suit, i));
-                        }
-                    }
-                }
-                Shuffle();
-            }
-        }
-
-        public class Hand
-        {
-            public string Name { get; set; }
-            public List<Card> Cards { get; set; }
-            public int LowAceCount { get; set; }
-            public int HandValue
-            {
-                get
-                {
-                    int score = 0;
-                    LowAceCount = Cards.FindAll(c => c.Value == 1).Count;
-                    foreach (Card card in Cards)
-                    {
-                        score += card.Value;
-                    }
-                    while (score < 12 && LowAceCount > 0)
-                    {
-                        LowAceCount--;
-                        score += 10;
-                    }
-                    return score;
-                }
-            }
-            public void Display()
-            {
-                Console.WriteLine("{0}'s Hand",Name);
-                foreach (Card card in Cards)
-                {
-                    Console.WriteLine(string.Format("{0} of {1}",card.Display,card.Suit));
-                }
-                Console.WriteLine(string.Format("Hand Value: {0}",HandValue));
-                Console.WriteLine();
-            }
-            public Hand(string name)
-            {
-                LowAceCount = 0;
-                Name = name;
-                Cards = new List<Card>();
-            }
-        }
-
         public static void Main(string[] args)
         {
+            var testDeck = new Deck();
+            var meme = testDeck.Cards.Distinct();
+            Console.WriteLine(meme.Count());
+
+            Console.ReadLine();
 
             bool game = true;
             string input;
--- src/blackjacktests/Class1.cs
+++ src/blackjacktests/Class1.cs
@@ -4,14 +4,165 @@ using System.Linq;
 using System.Threading.Tasks;
 using Xunit;
 
-namespace blackjacktests
+namespace blackjack.Tests
 {
     public class CardTests
     {
         [Fact]
-        public void TestConstructor()
+        public void TestConstructorClubsAce()
         {
-            
+            var myCard = new Card(CardSuit.Clubs, 1);
+
+            Assert.Equal(CardSuit.Clubs, myCard.Suit);
+            Assert.Equal(1, myCard.Score);
+            Assert.Equal(myCard.Display, CardDisplay.Ace);
+        }
+        [Fact]
+        public void TestConstructorHeartsTwo()
+        {
+            var myCard = new Card(CardSuit.Hearts, 2);
+
+            Assert.Equal(CardSuit.Hearts, myCard.Suit);
+            Assert.Equal(2, myCard.Score);
+            Assert.Equal(myCard.Display, CardDisplay.Two);
+        }
+        [Fact]
+        public void TestConstructorDiamondsThree()
+        {
+            var myCard = new Card(CardSuit.Diamonds, 3);
+
+            Assert.Equal(CardSuit.Diamonds, myCard.Suit);
+            Assert.Equal(3, myCard.Score);
+            Assert.Equal(myCard.Display, CardDisplay.Three);
+        }
+        [Fact]
+        public void TestConstructorSpadesFour()
+        {
+            var myCard = new Card(CardSuit.Spades, 4);
+
+            Assert.Equal(CardSuit.Spades, myCard.Suit);
+            Assert.Equal(4, myCard.Score);
+            Assert.Equal(myCard.Display, CardDisplay.Four);
+        }
+        [Fact]
+        public void TestConstructorClubsFive()
+        {
+            var myCard = new Card(CardSuit.Clubs, 5);
+
+            Assert.Equal(CardSuit.Clubs, myCard.Suit);
+            Assert.Equal(5, myCard.Score);
+            Assert.Equal(myCard.Display, CardDisplay.Five);
+        }
+        [Fact]
+        public void TestConstructorHeartsSix()
+        {
+            var myCard = new Card(CardSuit.Hearts, 6);
+
+            Assert.Equal(CardSuit.Hearts, myCard.Suit);
+            Assert.Equal(6, myCard.Score);
+            Assert.Equal(myCard.Display, CardDisplay.Six);
+        }
+        [Fact]
+        public void TestConstructorDiamondsSeven()
+        {
+            var myCard = new Card(CardSuit.Diamonds, 7);
+
+            Assert.Equal(CardSuit.Diamonds, myCard.Suit);
+            Assert.Equal(7, myCard.Score);
+            Assert.Equal(myCard.Display, CardDisplay.Seven);
+        }
+        [Fact]
+        public void TestConstructorSpadesEight()
+        {
+            var myCard = new Card(CardSuit.Spades, 8);
+
+            Assert.Equal(CardSuit.Spades, myCard.Suit);
+            Assert.Equal(8, myCard.Score);
+            Assert.Equal(myCard.Display, CardDisplay.Eight);
+        }
+        [Fact]
+        public void TestConstructorClubsNine()
+        {
+            var myCard = new Card(CardSuit.Clubs, 9);
+
+            Assert.Equal(CardSuit.Clubs, myCard.Suit);
+            Assert.Equal(9, myCard.Score);
+            Assert.Equal(myCard.Display, CardDisplay.Nine);
+        }
+        [Fact]
+        public void TestConstructorHeartsTen()
+        {
+            var myCard = new Card(CardSuit.Hearts, 10);
+
+            Assert.Equal(CardSuit.Hearts, myCard.Suit);
+            Assert.Equal(10, myCard.Score);
+            Assert.Equal(myCard.Display, CardDisplay.Ten);
+        }
+        [Fact]
+        public void TestConstructorDiamondsJack()
+        {
+            var myCard = new Card(CardSuit.Diamonds, 11);
+
+            Assert.Equal(CardSuit.Diamonds, myCard.Suit);
+            Assert.Equal(10, myCard.Score);
+            Assert.Equal(myCard.Display, CardDisplay.Jack);
+        }
+        [Fact]
+        public void TestConstructorSpadesQueen()
+        {
+            var myCard = new Card(CardSuit.Spades, 12);
+
+            Assert.Equal(CardSuit.Spades, myCard.Suit);
+            Assert.Equal(10, myCard.Score);
+            Assert.Equal(myCard.Display, CardDisplay.Queen);
+        }
+        [Fact]
+        public void TestConstructorClubsKing()
+        {
+            var myCard = new Card(CardSuit.Clubs, 13);
+
+            Assert.Equal(CardSuit.Clubs, myCard.Suit);
+            Assert.Equal(10, myCard.Score);
+            Assert.Equal(myCard.Display, CardDisplay.King);
+        }
+        [Fact]
+        public void TestConstructorBadValue()
+        {
+            Assert.Throws(typeof(ArgumentOutOfRangeException), () => {
+                var myCard = new Card(CardSuit.Clubs, 0);
+            });
+        }
+    }
+
+    public class DeckTests
+    {
+        [Fact]
+        public void TestConstructorCount()
+        {
+            var myDeck = new Deck();
+
+            Assert.Equal(52, myDeck.Cards.Count);
+        }
+        [Fact]
+        public void TestConstructorUnique()
+        {
+            var myDeck = new Deck();
+
+            Assert.Equal(52,myDeck.Cards.Distinct().Count());
+        }
+        [Fact]
+        public void TestShuffle()
+        {
+            var deck1 = new Deck();
+            var deck2 = new Deck();
+            for (int i = 0; i < deck1.Cards.Count; i++)
+            {
+                if (deck1.Cards[i] != deck2.Cards[i])
+                {
+                    return;
+                }
+            }
+            throw new Exception("Deck not properly shuffled!");
         }
     }
 }
